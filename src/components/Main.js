d864c74a2ce91e5d7aadc40e63a403af385e3a5e
mensahTribeWeb <ndm.acct85@gmail.com>
2022-10-31 14:08:03 -0500

    update css, completion

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+
 export default function Main() {
   return (
     <div className="main">
-      <ul>
+      <h1 className="main--title">Fun facts about React</h1>
+      <ul className="main--facts">
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
         <li>Has over 100K stars on Github</li>
